acfa17b4858184eaf2f39332e8d17a10ab72f131
Hoon Lee <yl.hoony@gmail.com>
2018-04-14 21:22:02 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,7 +10,7 @@ class RepositoriesController < ApplicationController
         req.params['client_id'] = 'd80d181bc323b63ff712'
         req.params['client_secret'] = 'f7852679b22cc112a4fed5f8e5ecc7c772b5304e'
         req.params['q'] = params[:query]
-        req.options.timeout = 0
+        req.options.timeout = 120
       end
 
       body = JSON.parse(@resp.body)
