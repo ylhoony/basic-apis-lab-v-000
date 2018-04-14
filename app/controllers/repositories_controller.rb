b5ffab8a2eceb92be1065dcd5f98bd28f62a86b4
Hoon Lee <yl.hoony@gmail.com>
2018-04-14 21:31:15 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,8 +7,8 @@ class RepositoriesController < ApplicationController
   def github_search
     begin
       @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-        req.params['client_id'] = 'd80d181bc323b63ff712'
-        req.params['client_secret'] = 'f7852679b22cc112a4fed5f8e5ecc7c772b5304e'
+        req.params['client_id'] = ''
+        req.params['client_secret'] = ''
         req.params['q'] = params[:query]
         req.options.timeout = 120
       end
