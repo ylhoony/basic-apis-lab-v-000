e3145862a0d9f0a7ee8966751aef11d16e22c769
Hoon Lee <yl.hoony@gmail.com>
2018-04-14 20:53:35 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,8 +3,8 @@ class RepositoriesController < ApplicationController
   def search
 
     Faraday.get 'https://api.github.com/user/repos' do |req|
-      req.params['client_id'] = ''
-      req.params['client_secret'] = ''
+      req.params['client_id'] = 'd80d181bc323b63ff712'
+      req.params['client_secret'] = 'f7852679b22cc112a4fed5f8e5ecc7c772b5304e'
     end
 
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
